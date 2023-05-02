c931c23ada6360346b4b0efa298e52382d84c9f0
chnspart <imchn24@gmail.com>
2023-05-02 16:41:16 +0600

    Next Image, Styles and Posts with authorId

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,11 +1,45 @@
 // import { z } from "zod";
 
+import { User } from "@clerk/nextjs/dist/api";
+import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUserForClient = (user: User) => {
+  return {id: user.id, 
+    username:user.username, 
+    profilePicture: user.profileImageUrl
+  }
+}
 
 export const postsRouter = createTRPCRouter({
   /* Get All Data */
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query( async ({ ctx }) => {
+    
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    })
+
+    const users = (await clerkClient.users.getUserList({
+      userId: posts.map(post => post.authorId),
+      limit: 100
+    })).map(filterUserForClient)
+
+    console.log(users)
+    
+    return posts.map((post) => {
+
+      const author = users.find(user => user.id === post.authorId)
+      
+      if (!author || !author.username) throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "Author for posts not found !" })
+
+      return {
+          post,
+          author: {
+            ...author,
+            username: author.username
+          }
+      }});
   }),
 
 });
